6f8c38195656b6eafd251342ce7e3a43bf3f0104
wykr0 <manuel.cruces@sansano.usm.cl>
2024-04-20 18:30:25 -0400

    Manuel Cruces 201704005-k

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function twoNumberSum(array, targetSum) { 
-    const result = [];
+function twoNumberSum(array, targetSum) {  //funcion que recibe el arreglo y la suma objetivo
+    const result = []; //arreglo vacio a retornar
 
-    array.forEach((element,index) => {
-        for (let temp = index + 1; temp < array.length; temp++) {
+    array.forEach((element,index) => { //por cada elemento del arreglo y su indice
+        for (let temp = index + 1; temp < array.length; temp++) { //desde el siguiente elemento de el arreglo
             const element2 = array[temp];
             if (element + element2 === targetSum){
                 result.push(element, element2);
@@ -10,10 +10,10 @@ function twoNumberSum(array, targetSum) {
 
         }
     })
-    return result
+    return result //retorna el arreglo
 }
 
-const array = [3, 5, -4, 8, 10, 1, -1, 5] 
+const array = [3, 5, -4, 8, 10, 1, -1, 5]  //areglo modificado para pruebas
 const targetSum = 10
 const result = twoNumberSum(array, targetSum)
 console.log(result)
